b396b0766e82f80356033762288cae0b58d71c14
chagan <chrishagan23@gmail.com>
2016-01-20 14:34:25 -0600

    Adding slider functionality

--- app_config.py
+++ app_config.py
@@ -93,7 +93,7 @@ DEBUG = True
 """
 COPY EDITING
 """
-COPY_GOOGLE_DOC_KEY = '0AlXMOHKxzQVRdHZuX1UycXplRlBfLVB0UVNldHJYZmc'
+COPY_GOOGLE_DOC_KEY = '1kfduIN1aQ-r16FNtrqdeiwxEC_JjDWotvG49RunMZ-U'
 COPY_PATH = 'data/copy.xlsx'
 
 """
--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,6 @@ var onDocumentLoad = function(e) {
     $commentCount = $('.comment-count');
 
     renderExampleTemplate();
-    getCommentCount(showCommentCount);
 
     SHARE.setup();
 }
@@ -43,5 +42,73 @@ var showCommentCount = function(count) {
         $commentCount.next('.comment-label').text('Comments');
     }
 }
+
+var onPageLoad = function() {
+    $('.section').css({
+      'opacity': 1,
+      'visibility': 'visible',
+    });
+};
+
+/*
+ * DInitialize fullpage.
+ */
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        controlArrows: true,
+        css3: true,
+        fixedElements: '#nav',
+        loopHorizontal: false,
+        autoScrolling: false,
+        keyboardScrolling: false,
+        scrollingSpeed: 0,
+        afterRender: onPageLoad,
+        onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
+            var leavingSlide = $(this);
+            $.fn.fullpage.setScrollingSpeed(800);            
+        },
+        onLeave: function( index, nextIndex, direction){
+            var leavingSlide = $(this);
+            var scrollSpeed = 0;
+            console.log(index, nextIndex, direction)
+            if (index == 1) {
+                scrollSpeed = 800;
+            }
+            $.fn.fullpage.setScrollingSpeed(scrollSpeed);          
+        }
+    });
+
+    $('.start.btn').click(function(){
+        $.fn.fullpage.moveTo(2,0)
+    });
+
+    $('.return').click(function(){
+        $.fn.fullpage.moveTo(2, 0)
+    });
+
+    $('.flip').click(function(){
+        $slide = $(this).parents( ".slide" )
+        $slide.find('.front').toggleClass('active-side');
+        $slide.find('.back').toggleClass('active-side');
+        var bg = $slide.find('.active-side').data('background');
+        $slide.css('background-image','url(assets/'+bg+')');
+        console.log($slide)
+    });
+
+    for (var i = 0; i < COPY.personas.length; i++) {
+        (function(j) {
+            var personaID = COPY.personas[j]['id']
+            var personaBtn = '.' + personaID + '-start'
+
+            console.log(personaID,personaBtn)
+            
+            $(personaBtn).click(function(){
+                $.fn.fullpage.moveTo(personaID)
+            });
+
+        })(i);
+    }
+});
 
 $(onDocumentLoad);
